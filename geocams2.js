0894ec4e62d5680da93910aefae5ad97b9863ddf
Spice_9 <105491758+Spice9@users.noreply.github.com>
2025-03-21 04:28:34 +0530

    Update geocams2.js

--- geocams2.js
+++ geocams2.js
@@ -11,37 +11,66 @@
 // @require http://code.jquery.com/jquery-3.4.1.min.js
 // ==/UserScript==
 
-function changeFOV()
-{
-    geofs.camera.setFOV("1.2");
-    document.getElementById("changeFOV").innerHTML = "";
+// Define the changeFOV function globally
+function changeFOV() {
+
+    let currentFOV = geofs.camera.currentFOV;
+    let targetFOV;
+    if(currentFOV>1.2)
+    {
+    targetFOV=5;
+    }
+    else{
+        targetFOV=1.2;
+    }
+    let steps = currentFOV<targetFOV?0.1:-0.1;
+    let interval = setInterval(function(){
+    currentFOV+=steps;
+    geofs.camera.setFOV(currentFOV);
+
+    if((steps>0&&currentFOV>=targetFOV)||(steps<0&&currentFOV<=targetFOV)){
+        clearInterval(interval);
+        document.getElementById("changeFOV").innerHTML = "FOV Adjusted!";
+    }
+    },50);
+    geofs.camera.animations.orbitHorizontal.active==1
 }
 
+// Dynamically create the panel div
 let listdiv = document.createElement("div");
-    listdiv.setAttribute("data-noblur", "true");
-    listdiv.setAttribute("data-onshow", "{geofs.initializePreferencesPanel()}");
-    listdiv.setAttribute("data-onhide", "{geofs.savePreferencesPanel()}");
-    listdiv.setAttribute("class", "geofs-list geofs-toggle-panel geofs-livery-list geofs-visible")
-    listdiv.setAttribute("onClick", "changeFOV()")
-    listdiv.innerHTML = '<h3>GeoCams</h3><ul>Adjust fov</ul> <script type = "type/javascript">function changeFOV(){geofs.camera.setFOV("1.2");}</script> <button class="mdl-button mdl-js-button geofs-f-standard-ui geofs-mediumScreenOnly" onClick = "changeFOV()">Change fov</button>'
-    document.getElementsByClassName("geofs-ui-left")[0].appendChild(listdiv);
+listdiv.setAttribute("data-noblur", "true");
+listdiv.setAttribute("data-onshow", "{geofs.initializePreferencesPanel()}");
+listdiv.setAttribute("data-onhide", "{geofs.savePreferencesPanel()}");
+listdiv.setAttribute("class", "geofs-list geofs-toggle-panel geofs-livery-list geofs-visible");
 
+// Adjusted inner HTML with no conflicting script tag
+listdiv.innerHTML = '<h3>GeoCams</h3><ul>Adjust FOV</ul><button id="changeFOVButton" class="mdl-button mdl-js-button geofs-f-standard-ui geofs-mediumScreenOnly">Change FOV</button>';
+
+// Append the panel div to the UI
+document.getElementsByClassName("geofs-ui-left")[0].appendChild(listdiv);
+
+// Add the event listener to the button after it’s created
+document.getElementById("changeFOVButton").addEventListener("click", changeFOV);
+
+// Create a button to toggle the panel
 let button1 = document.createElement("div");
-   button1.setAttribute("data-noblur", "true");
-   button1.innerHTML = '<button class="mdl-button mdl-js-button geofs-f-standard-ui geofs-mediumScreenOnly" data-tooltip-classname="mdl-tooltip--top" id = "ebutton" onClick = "changeFOV()">Button</button>'
- //  document.getElementById("ListPane").appendChild(button1);
-    // Button for panel
-    let buttonDiv = document.createElement("div");
-    buttonDiv.innerHTML = '<button class="button class="mdl-button mdl-js-button geofs-f-standard-ui geofs-mediumScreenOnly" data-toggle-panel=".geofs-livery-list"  data-tooltip-classname="mdl-tooltip--top"  tabindex="0" id="cambutton" size = "50%" onClick = "listPanel()">  Geocams  </button>'
-    document.body.appendChild(buttonDiv);
-    let element = document.getElementById("cambutton");
-    if (geofs.version >= 3.6){
-        document.getElementsByClassName("geofs-ui-bottom")[0].insertBefore(element, document.getElementsByClassName("geofs-ui-bottom")[0].children[4]);
-    } else {
-        document.getElementsByClassName("geofs-ui-bottom")[0].insertBefore(element, document.getElementsByClassName("geofs-ui-bottom")[0].children[3]);
-    }
+button1.setAttribute("data-noblur", "true");
+button1.innerHTML = '<button class="mdl-button mdl-js-button geofs-f-standard-ui geofs-mediumScreenOnly" data-tooltip-classname="mdl-tooltip--top" id="ebutton">Button</button>';
 
-function listPanel(){
+// Append the button for the panel toggle
+let buttonDiv = document.createElement("div");
+buttonDiv.innerHTML = '<button class="mdl-button mdl-js-button geofs-f-standard-ui geofs-mediumScreenOnly" data-toggle-panel=".geofs-livery-list" data-tooltip-classname="mdl-tooltip--top" tabindex="0" id="cambutton" size="50%">Geocams</button>';
+document.body.appendChild(buttonDiv);
+
+// Insert the button into the appropriate UI section based on the GeofS version
+let element = document.getElementById("cambutton");
+if (geofs.version >= 3.6) {
+    document.getElementsByClassName("geofs-ui-bottom")[0].insertBefore(element, document.getElementsByClassName("geofs-ui-bottom")[0].children[4]);
+} else {
+    document.getElementsByClassName("geofs-ui-bottom")[0].insertBefore(element, document.getElementsByClassName("geofs-ui-bottom")[0].children[3]);
+}
+
+// Function to clear list panel (if needed)
+function listPanel() {
     document.getElementById("ListPanel").innerHTML = "";
-
 }
